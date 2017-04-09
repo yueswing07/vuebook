535a8cb7dc54a8478c2c941601f471c170774223
Tobias Stosius <stosius.tobias@gmail.com>
2017-04-09 02:27:35 +0200

    [TASK] Add excuses from database

--- src/components/MissedTimes.vue
+++ src/components/MissedTimes.vue
@@ -1,14 +1,29 @@
 <template>
-  <div class="overview-container">
-    <h2>Fehlzeiten</h2>
-    <select class='missing-times-list'>
-      <option v-for='fehlzeit in databaseValue' class='list-item'>
-        <p>Datum: {{fehlzeit.date}}</p>
-        <p>Dauer: {{fehlzeit.duration}}</p>
-        <p>Stunde: {{fehlzeit.lesson}}</p>
-        <p>Status: {{fehlzeit.status}}</p>
-      </option>
-    </select>
+  <div class="overview-container container">
+    <div class="row">
+        <h2>Fehlzeiten</h2>
+    </div>
+    <div class="row">        
+        <select class='missing-times-list'>
+            <option v-for='fehlzeit in databaseValue' class='list-item'>
+                <p>Datum: {{fehlzeit.date}}</p>
+                <p>Dauer: {{fehlzeit.duration}}</p>
+                <p>Stunde: {{fehlzeit.lesson}}</p>
+                <p>Entschuldigung: {{fehlzeit.description}}</p>
+                <p>Status: {{fehlzeit.status}}</p>
+            </option>
+        </select>
+    </div>
+    <div class="row">
+        <input type="date">
+        <input type="time">
+        <select name="" id="">
+            <option value="" v-for='n in 12'> Stunde {{n}}</option>
+        </select>
+        <select name="" id="">
+            <option value="" v-for='excuse in excuses' v-if='excuse'>{{excuse}}</option>
+        </select>
+    </div>
   </div>
 </template>
 
@@ -29,16 +44,20 @@
             currentuser: function(){
                 if (this.currentuser.uid !== 'undefined') {
                     var that = this
-                    firebase.database().ref('debug/'+this.currentuser.uid+'/fehlzeiten/').on('value', function(snapshot){
+                    firebase.database().ref('debug/'+this.currentuser.uid+'/fehlzeiten/').on('value', (snapshot) => {
                         awesome.debug('servere','MissedTimes.js','Change ref from "debug" to "user"')
                         that.databaseValue = snapshot.val()
+                    })
+                    firebase.database().ref('resources/excuses/').on('value', (snapshot) => {
+                        awesome.debug('debug','MissedTimes.vue','Found excuses',snapshot.val())
+                        that.excuses = snapshot.val()
                     })
                 }
             }
         },
         data() {
             return {
-                message: '',
+                excuses: '',
                 databaseValue: ''
             }
         },
