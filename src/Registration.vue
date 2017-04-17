d0d7ec3a0754b84cf3292ddd22db16c3fa7ad13f
Tobias Stosius <stosius.tobias@gmail.com>
2017-04-17 22:45:49 +0200

    [TASK] Add cancel button

--- src/Registration.vue
+++ src/Registration.vue
@@ -59,7 +59,8 @@
                         <input type="text" id='instructor_name' placeholder='Tel.' v-model='value_instructor_phone'>
                     </li>
                 </ul>
-                <input type="button" class='btn btn-primary pull-right' value='Senden' @click='sendData()'>
+                <input type="button" class='btn btn-primary' value='Senden' @click='sendData()'>
+                <input type="button" class='btn btn-danger' value='Abbrechen' @click='candelRegistration()'>
             </div>            
         </div>
     </div>
